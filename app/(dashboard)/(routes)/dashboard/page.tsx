6f1f669dc938159a8532e6bcfb528a191235472a
mmiiprpkr <mmiiprpkr@gmail.com>
2024-03-08 02:08:53 +0700

    fix it

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,7 +3,7 @@
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { KindeState } from "@kinde-oss/kinde-auth-nextjs/dist/types";
 
-import { useMutation, useQuery } from "convex/react";
+import { useMutation, useQuery as Query } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useRouter } from "next/navigation";
 import { Team } from "@/types";
@@ -11,43 +11,39 @@ import { Loader2 } from "lucide-react";
 
 const Dashboardpage = () => {
   const { user }: KindeState = useKindeBrowserClient();
-  const createUser = useMutation(api.user.createUser)
+  const createUser = useMutation(api.user.createUser);
   const router = useRouter();
 
-  const result =  useQuery(api.user.getUser, { email: user?.email as string });
-  const team: Team[] | undefined = useQuery(api.team.getTeam, {
-    email: user?.email as string,
+  const result = Query(api.user.getUser, { email: user?.email as string });
+  const team: Team[] | undefined = Query(api.team.getTeam, {
+    email: user === null ? "" : (user.email as string),
   });
 
-  console.log(result, team)
-
-  if (result === undefined || team === undefined) {
+  if (user === null || result === undefined || team === undefined) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <Loader2 className="h-5 w-5"/>
+        <Loader2 className="h-5 w-5" />
       </div>
-    )
+    );
   }
 
-  console.log(team);
-  
   if (!result[0]._id) {
     createUser({
       name: user?.given_name!,
       email: user?.email!,
-      image: user?.picture!
+      image: user?.picture!,
     }).then((resp) => {
       console.log(resp);
-    })
+    });
   }
 
-  if (team.length) { 
-    return router.push(`/dashboard/${team[0]._id}`)
-  };
-  
+  console.log(team);
+
   if (!team.length) {
     return router.push("/teams/create");
   }
+
+  return router.push(`/dashboard/${team[0]._id}`);
 };
 
 export default Dashboardpage;
